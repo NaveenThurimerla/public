d7e0487af36fb7c942ae58ecf02a92d05f596018
Naveen Thurimerla <49746188+NaveenThurimerla@users.noreply.github.com>
2020-04-27 12:23:55 +0530

    Update _etl_pandas.py

--- _etl_pandas.py
+++ _etl_pandas.py
@@ -3,13 +3,14 @@ import pandas as pd
 #Read the data from CSV file
 df = pd.read_csv("https://raw.githubusercontent.com/NaveenThurimerla/public/master/dummyemployee.csv")
 
-#Printing the original String
+#Printing the original DF
 print(df)   
-#storing all the c
+
+#storing all the columns
 headers = df.columns
 
-#Converting integer vvalues to string
-df[df.columns[0]]=df[df.columns[0]].astype(str)
+#Converting  all the values to string
+df=df[:].astype(str)
 
 #Adding derived key to DataFrame
 df['mainkey']=df[df.columns[0]]+'_'+df[df.columns[1]]
